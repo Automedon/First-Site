023e226fe0f9925dd1024b6ca5180a777a8c40d7
Automedon <42299107+Automedon@users.noreply.github.com>
2018-10-11 21:24:42 +0700

    update for code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-
 import React from "react";
 import Slider from "react-slick";
 import ReactDom from 'react-dom';
 
 class SimpleSlider extends React.Component {
-		render() {
+	render() {
 		var settings = {
 			dots: true,
 			infinite: true,
@@ -17,22 +16,27 @@ class SimpleSlider extends React.Component {
 		return (
 			<Slider {...settings}>
 				<div className="col-12">
-					<img src={require("./icons/664063.jpg")} alt="" className="img-fluid rounded mx-auto d-block" />
-					<div className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At debitis error in, nulla omnis pariatur perspiciatis soluta tenetur! Facilis, inventore.</div>
+					<img src={require("./icons/664063.jpg")} alt="" className="img-fluid rounded mx-auto d-block"/>
+					<div className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At debitis error in,
+						nulla omnis pariatur perspiciatis soluta tenetur! Facilis, inventore.
+					</div>
 				</div>
 				<div className="col-12">
-					<img src={require("./icons/cat1.jpg")} alt="" className="img-fluid rounded mx-auto d-block" />
-					<div className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur in molestiae neque quia unde?</div>
+					<img src={require("./icons/cat1.jpg")} alt="" className="img-fluid rounded mx-auto d-block"/>
+					<div className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur
+						in molestiae neque quia unde?
+					</div>
 				</div>
 				<div className="col-12">
-					<img src={require("./icons/17683912355702337432.jpg")} alt="" className="img-fluid rounded mx-auto d-block " />
-					<div  className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, vero!</div>
+					<img src={require("./icons/17683912355702337432.jpg")} alt="" className="img-fluid rounded mx-auto d-block "/>
+					<div className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, vero!</div>
 				</div>
 
 			</Slider>
 		);
 	}
 }
+
 ReactDom.render(
 	<div>
 		<SimpleSlider/>
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const webpack= require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 
-let buildEntryPoint = function(entryPoint){
+let buildEntryPoint = function (entryPoint) {
 	return [
 		'webpack-dev-server/client?http://localhost:8080',
 		entryPoint
@@ -11,55 +10,58 @@ let buildEntryPoint = function(entryPoint){
 };
 
 module.exports = {
-    mode: "development",
+	mode: "development",
 	entry: {
 		file1: buildEntryPoint('./src/index.js'),
 		file2: buildEntryPoint('./src/index2.js')
 	},
-    plugins: [
+	plugins: [
 
 		/*	new HtmlWebpackPlugin({
 				inject: false,
 				template: path.join(__dirname, '/public/index.html')
 			}),*/
-   ],
-    output: {
-        filename: '[name].js',
+	],
+	output: {
+		filename: '[name].js',
 
-        path: path.resolve(__dirname, './public/dist'),
-			library: "[name]"
-		},
+		path: path.resolve(__dirname, './public/dist'),
+		library: "[name]"
+	},
 
 
-    module: {
+	module: {
 
-        rules: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", options: {
-                    presets: ['react','env']
-                }, include: path.join(__dirname, 'src')},
-					{ test: /\.(gif|png|jpe?g|svg)$/i,
-						use: [
-							'file-loader',
-							{
-								loader: 'image-webpack-loader',
-								options: {
-									bypassOnDebug: true, // webpack@1.x
-									disable: true, // webpack@2.x and newer
-								},
-							},
-						],
-					}
-        ]
+		rules: [
+			{
+				test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", options: {
+					presets: ['react', 'env']
+				}, include: path.join(__dirname, 'src')
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true, // webpack@1.x
+							disable: true, // webpack@2.x and newer
+						},
+					},
+				],
+			}
+		]
 
-    },
-    devServer: {
-        host:'localhost',
-        port:8080,
-        contentBase: path.join(__dirname, 'public'),
-				compress: true,
-        watchContentBase: true,
-        hot: true,
-        inline: true
-    }
+	},
+	devServer: {
+		host: 'localhost',
+		port: 8080,
+		contentBase: path.join(__dirname, 'public'),
+		compress: true,
+		watchContentBase: true,
+		hot: true,
+		inline: true
+	}
 
 };
